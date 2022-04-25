52abc4fbf2a5d8e7c80da5d1d5d37d0bc2a326a4
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2022-04-25 15:17:57 +0300

    create folder adn cleaning files

---
+++ lessons_2/script.js
@@ -0,0 +1,67 @@
+
+'use strict';
+
+console.log(leftBorderWith);
+
+var leftBorderWith =1;
+
+let second=2;
+
+const pi =3.14;
+
+console.log(leftBorderWith);
+
+var number = 5;
+var string ="Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj={};
+
+console.log(4/0);
+console.log('string'*9);
+
+let persone = {
+    name: "John",
+    age:25,
+    isMarried: false
+};
+
+console.log(persone.name);
+
+
+
+let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+
+console.log(arr[1]);
+
+// alert("hello world!")
+
+// let answer = confirm("Are you here ?");
+
+// console.log(answer);
+
+// let answer_2 = prompt("Are you 18?", "Да");
+
+console.log(typeof(arr));
+
+console.log(arr + " - object")
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+    console.log("2" === 2);
+
+let isCheked=false,
+    isClose=false;
+
+    console.log(isCheked || isClose);
