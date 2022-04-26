13a063d91e6354722f074b5c8be66622329d9320
Vadim <Vadim_milo.net@mail.ru>
2022-04-26 15:12:01 +0300

    Operator IF and SWITCH

---
+++ lessons_4/js/main.js
@@ -0,0 +1,25 @@
+
+'use strict';
+
+let money = prompt("What is your monthly budget?"),
+    time = prompt("Enter a date in the format YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optinalExpenses: {},
+    income : [],
+    savings: false
+};
+
+let a1 = prompt("Enter a mandatory expense item for this month"),
+    a2 = prompt("How much will it cost?"),
+    a3 = prompt("Enter a mandatory expense item for this month"),
+    a4 = prompt("How much will it cost?");
+
+appData.expenses.a1=a2;
+appData.expenses.a3=a4;
+
+alert("answer to the first question " + a1, "answer to the two question " + a2),
+alert("Budget in one day " + money/30);
