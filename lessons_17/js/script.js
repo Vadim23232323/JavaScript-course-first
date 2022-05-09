75c594540d4aeb6dfcd04e9c9479567efa45c493
Vadim <Vadim_milo.net@mail.ru>
2022-05-09 18:54:23 +0300

    setTimeout и setInterval

---
+++ lessons_17/js/script.js
@@ -0,0 +1,15 @@
+// let timerId = setTimeout(sayHello, 3000);
+// clearTimeout(timerId);
+ 
+// let timerId = setInterval(sayHello, 3000);
+// // clearTimeout(timerId);
+
+// function sayHello() {
+//     alert('Hello world!')
+// }
+
+// let timerID = setTimeout(function log() {
+//     console.log('hello');
+//     setTimeout(log, 2000);
+// });
+
